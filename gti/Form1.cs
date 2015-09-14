bb481ea2d0fc7c3f70062b4e2bc4d2b62a00717a
jzhfighting <312456051@qq.com>
2015-09-14 15:47:20 +0800

    登出Good

--- gti/Form1.cs
+++ gti/Form1.cs
@@ -16,5 +16,10 @@ namespace gti
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = "Good!";
+        }
     }
 }
